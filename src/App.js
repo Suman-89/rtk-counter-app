c5740afa37828f35bdefd2de627f4a5c6b1a94a3
Suman-89 <suman.mukherjee01@gmail.com>
2023-07-15 01:19:57 +0530

    browserRouter shifted to index.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
 import "./App.css";
 import ChildComp from "./component/ChildComp";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import UserList from "./component/crudApp/UserList";
 import NavBar from "./component/NavBar";
 import SingleUser from "./component/crudApp/SingleUser";
-
 
 
 function App() {
@@ -14,14 +13,12 @@ function App() {
   return (
     <div className="App">
       <div className="container my-4">
-        <BrowserRouter>
         <NavBar/>
         <Routes>
           <Route exact path="/" element={<ChildComp/>}></Route>
           <Route exact path="/user" element={<UserList/>}></Route>
           <Route exact path="/singleuser/:vid" element={<SingleUser/>}></Route>
         </Routes>
-        </BrowserRouter>
       </div>     
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -5,14 +5,17 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
